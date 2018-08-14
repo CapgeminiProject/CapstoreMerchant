9635650e3c87bf70b1e52526e03611572dbf6cfa
Lalit Nikhare <lalitn246@gmail.com>
2018-08-14 11:34:21 +0530

    Initial Project Structure

---
+++ src/main/java/com/capgemini/merchantstore/MerchantstoreApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.merchantstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchantstoreApplication.class, args);
+	}
+}
