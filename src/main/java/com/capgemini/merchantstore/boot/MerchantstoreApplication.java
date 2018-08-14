d7480a75697fe38c52f05683119ccfb45851817a
Lalit Nikhare <lalitn246@gmail.com>
2018-08-14 14:17:11 +0530

    Renamed Packages

--- src/main/java/com/capgemini/merchantstore/boot/MerchantstoreApplication.java
+++ src/main/java/com/capgemini/merchantstore/boot/MerchantstoreApplication.java
@@ -1,4 +1,4 @@
-package com.capgemini.merchantstore;
+package com.capgemini.merchantstore.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
